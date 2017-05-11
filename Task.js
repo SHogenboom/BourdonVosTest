c6b72d2dc4eae1d6cc8e169174a1c5124a615b1b
SHogenboom <hogen.boom@hotmail.com>
2017-05-11 10:46:10 +0200

    Random Figure Presentation
    Creates a full page with random figures containing 3 /4 /5 dots. Only
    three configurations possible

--- Task.js
+++ Task.js
@@ -3,44 +3,64 @@
 // Author: Sally A.M. Hogenboom
 // Version Control via Git
 
- // presenting multiple pictures through appending to existing element <div id = 'stimuli'>;
-     function stimuliPresentation (stimID, stimObject) {
-     	// stimID = id tag of the image that needs to be presented;
-     	// stimObject = id tag of <div> to which the additional pictures should be appended;
-     	
-     	// Determine size of window, using || allows for this function to work in Explorer and other browsers;
-          var winWidth = window.innerWidth												
-                    || document.documentElement.clientWidth  || document.body.clientWidth;
-          var winHeight = window.innerHeight 												
-                    || document.documentElement.clientHeight || document.body.clientHeight;
+function randomPresentation (stimID, stimObject, targetHeight, targetWidth) {
+     // Goal: to randomly present  figures with 3,4, or 5 dots across the entire available window
+            // stimID = id tag of the empty image object that needs to be presented / changed;
+            // stimObject = id tag of <div> to which the additional pictures should be appended;
+            // targetHeight = height of the stimulus picture;
+            // targetWidth = width of the stimulus picture;
+     
+    // DETERMINE MAX AMOUNT OF PICTURES TO PRESENT
+            // Determine size of window, using || allows for this function to work in Explorer and other browsers;
+                    var winWidth = window.innerWidth												
+                         || document.documentElement.clientWidth  || document.body.clientWidth;
+                    var winHeight = window.innerHeight 												
+                         || document.documentElement.clientHeight || document.body.clientHeight;
                     
-         // Determine size of stimulus (i.e. the picture / grid of dots that is to be presented
-          var stimWidth = document.getElementById(stimID).width;
-          var stimHeight = document.getElementById(stimID).height;
+            // Determine size of stimulus (i.e. the picture / grid of dots that is to be presented
+                 var stimWidth = document.getElementById(stimID).width;
+                 var stimHeight = document.getElementById(stimID).height;
           
-          // Calculate  max number of pictures to fit the window
-          var totalPictures = (Math.floor(winWidth / stimWidth)) * (Math.floor(winHeight / stimHeight)); //Math.floor rounds the outcome down to an integer
-          
-          // Present more than 1 stimulus by adding the totalPictures number to an existing HTML object
-         for (i = 1; i < totalPictures; ){
-              // create new image object with appropriate values
-              var stimAdd = new Image();
-              stimAdd.src = document.getElementById(stimID).src;
-              stimAdd.alt = document.getElementById(stimID).alt;
-              stimAdd.width = document.getElementById(stimID).width;
-              stimAdd.height = document.getElementById(stimID).height;
-              
-              // append object to existing stimObject
-              document.getElementById(stimObject).appendChild(stimAdd);
-              
-            i = i + 1;
-          }  // end picture creation loop
-      } // end stimuli creation function
+            // Calculate  max number of pictures to fit the window
+                 var totalPictures = ((Math.floor(winWidth / stimWidth)) * (Math.floor(winHeight / stimHeight))) + 1; //Math.floor rounds the outcome down to an integer. +1 because first img object gets hidden so need to display an extra
 
-// initate function, input 'name of stimulus <img> id' & name of HTML object <div> id
-stimuliPresentation('stim', 'stimuli');
+    // APPEND RANDOM PICTURES
+            for (i = 1; i < totalPictures;) {
+                   // SELECT RANDOM FIGURE    
+                        function getRandomInt(min, max) {
+                            // GOAL: get a random integer in a range from min to max, in this case 3 - 5 dots.
+                            return Math.floor(Math.random() * (max - min + 1) + min);
+                         } // end RandomInt fucntion
 
- 
+                        var dots = getRandomInt(3,5); // set amount of dots
+                        // document.getElementById('testing').innerHTML = dots; // TEST
     
+                   if (dots == 3) {
+                            // GOAL: set corresponding source
+                            var source = '/Users/Sally/Documents/Universiteit/Master/9-Programming_NextStep/BourdonVosTest/Stimuli/Dots3.png'; 
+                    } else if (dots == 4) {
+                            var source = '/Users/Sally/Documents/Universiteit/Master/9-Programming_NextStep/BourdonVosTest/Stimuli/Dots4.png';
+                    } else {
+                            var source = '/Users/Sally/Documents/Universiteit/Master/9-Programming_NextStep/BourdonVosTest/Stimuli/Dots5.png'; 
+                    } // end if presenting 3/4/5 dot picture source
+
+                // APPEND PICTURE
+                    // appending picture to stimObject
+                     var stimAdd = new Image ();
+                     stimAdd.src = source;
+                     stimAdd.width = targetWidth;
+                     stimAdd.height = targetHeight;
+                     stimAdd.alt = "Dots" + dots;
+                
+                     document.getElementById(stimObject).appendChild(stimAdd);
+                
+             i = i + 1;      
+            } // END LOOP  "Append Random Picture" 
+     
+} // end of randomPresentation function
+
+// initialize function
+randomPresentation ('stim', 'stimuli', 50, 50);
+ 
     
     
