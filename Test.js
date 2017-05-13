16588d2786af0a56f21d25df1c879b36b2919300
SHogenboom <hogen.boom@hotmail.com>
2017-05-13 13:40:24 +0200

    TASK_final without timing
    Functioning task without timing. includes balance of 3 / 4 / 5 dot
    figures across the window that are responsive and create random
    configurations of dots.
    Needs (timing) and (tracking of first response per canvas).

--- Test.js
+++ Test.js
@@ -20,7 +20,7 @@ function stimulusPresentation (appendObject, sizeCirckel, verticalCirckles, hori
          
     // DRAW 1st CANVAS + FIGURE (to be able to determine canvas size)
         createCanvas(appendObject, "Canvas1");                                                                      // create new responsive canvas
-        randomFigure ("Canvas1", 10,10,sizeCirckel, horizontalCirckles, verticalCirckles);       // draw dot figure containing 3,4, or 5 dots
+        randomFigure ("Canvas1", 10,10,sizeCirckel, horizontalCirckles, verticalCirckles, getRandomInt(3, 5));       // draw figure containing 3/4/5 dots
         
     // DETERMINE MAX AMOUNT OF FIGURES TO PRESENT
             // Determine size of window, using || allows for this function to work in Explorer and other browsers (from internet);
@@ -35,15 +35,29 @@ function stimulusPresentation (appendObject, sizeCirckel, verticalCirckles, hori
           
             // Calculate max number of pictures to fit the window
                  var totalPictures = ((((Math.floor(winWidth / stimWidth)) -1) * ((Math.floor(winHeight / stimHeight))))) +1; //Math.floor rounds the outcome down to an integer. +1 because first img object gets hidden so need to display an extra
-
+            
+            // set random order 3/4/5 dots
+                var arrayDots = [];
+                      var amountFigures = totalPictures / 3;
+                   
+                    for (e = 3; e < 6; e++) {
+                        for (r = 0; r < amountFigures; r++) {           // append 1/3 of totalPictures with 3 dots, 1/3 4 dots, 1/3 5 dots
+                            arrayDots.push(e);
+                        } // END appending integers 1/3 of totalPictures
+                    } // END appending 3/4/5 dots   
+                shuffleArray(arrayDots);                                    // shuffle to create random order
+            
         // TEST
-            // document.getElementById("testing").innerHTML = ((Math.floor(winWidth / stimWidth))) -1;    
+           // document.getElementById("testing").innerHTML = ((Math.floor(winWidth / stimWidth)) -1);    
+            // window.alert(arrayDots);
                     
         // LOOP CREATION OF MAX AMOUNT FIGURES
             for (z = 2; z < totalPictures; z++) {                // start at 2 because first canvas was already created
                 var ID = "Canvas" + z;                              // Change .id for each newly created canvas
                 createCanvas (appendObject, ID);            // call upon createCanvas  (see below) to create responsive canvas
-                randomFigure (ID, 10,10,sizeCirckel, horizontalCirckles, verticalCirckles); // call upon randomFigure (see below) to create random dot figure
+                
+                var dots = arrayDots[z-2];
+                randomFigure (ID, 10,10,sizeCirckel, horizontalCirckles, verticalCirckles, dots); // call upon randomFigure (see below) to create random dot figure
             } // END FOR LOOP  
 } // END stimulusPresentation FUNCTION
 
@@ -132,7 +146,7 @@ function createCanvas (appendObject, canvasID) {
         //drawGrid (10,10,5, 3, 3, "Canvas1");
 
  
- function randomFigure (canvasID, posX,posY,sizeCirckel, horizontalCirckles, verticalCirckles) {
+ function randomFigure (canvasID, posX,posY,sizeCirckel, horizontalCirckles, verticalCirckles, dots) {
     // GOAL: to create a random figure containing 3 - 4 - 5 black dots 
             // canvasID: name of canvas on which to draw (itterated in stimulusPresentation)
             // posX: set at 10 (needs to be deleted)
@@ -140,11 +154,8 @@ function createCanvas (appendObject, canvasID) {
             // sizeCirckel: change size of dots (currently only works for values < 6)
             // horizontalCirckles: width of imaginary grid of dots (i.e. possible X positions)
             // verticalCirckles: height of imaganinary grid of dots (i.e. possible Y positions)
-    
-      // PRESENT 3/4/5 DOTS?
-            var dots = getRandomInt(3,5); // set amount of dots between 3 & 5 (= specified in Bourdon-Vos Task)
-            // document.getElementById('testing').innerHTML = dots; // TEST
-        
+            // dots: amount of dots to be drawn (randomized in stimulusPresentation)
+     
     // SET VARIABLES
         var distance = posX + sizeCirckel;                                                              // distance between dots, currently not included in position calculations
          canvasWidth = ((distance * horizontalCirckles) + sizeCirckel) + posX;      // size of canvas bigger than range for drawing dots (recalculate)
@@ -174,36 +185,57 @@ function createCanvas (appendObject, canvasID) {
             ctx.fill();                                                                         // execute
         } // END blackDot FUNCTION                                    
     
-    // SET RANDOM X POSITIONS
-        var arrayXPos = [];                                                         // create empty array
-            for (q = 1; q < (verticalCirckles + 1); q++) {    
-                for (k = 1; k < (horizontalCirckles + 1); k++) {         // positions for the amount of horizontalCirckles specified (+1 because k = 1)
-                    var posX = ((3 * sizeCirckel) * k);                       // (should be changed depending on canvas & circkel size)
-                    arrayXPos.push(posX);                                      // append new position to array containing all X positions
-                } // END horizontal LOOP
-            } // END vertical LOOP
-        shuffleArray(arrayXPos);                                                // randomize order of X positions (to allow for random dot placement)
-        // window.alert(arrayXPos);                                            // TEST
-     
-    // SET RANDOM Y POSITIONS
-        var arrayYPos = [];                                                         // create empty array
-            for (w = 1; w < (horizontalCirckles + 1); w++) { 
-                for (l = 1; l < (verticalCirckles + 1); l++) {                // positions for the amount of verticalCirckles specified (+1 because l = 1)
-                    var posY = ((3 * sizeCirckel) * l);                         // change depending on circkelSize
-                    arrayYPos.push(posY);                                       // append new position to array containing all Y position
+    // FUNCTION _ POSITION GENERATOR
+    function positionGrid (verticalCirckles, horizontalCirckles, sizeCirckel) { 
+        // GOAL: create an X and Y coordinate array depending on the amount of circkles that was specified 
+                // verticalCirckles: height of grid
+                // horizontalCirckles: width of grid
+                // sizeCirckel: size of the circkles for between circkel distance
+                
+        // possible X positions
+             arrayXPos = [];                                                                // create empty array accesible outside function
+                for (q = 1; q < (verticalCirckles + 1); q++) {                 // repeat X coordinates for all coordinates of Y
+                    for (k = 1; k < (horizontalCirckles + 1); k++) {         // positions for the amount of horizontalCirckles specified (+1 because k = 1)
+                        var posX = ((3 * sizeCirckel) * k);                       // (should be changed depending on canvas & circkel size)
+                        arrayXPos.push(posX);                                      // append new position to array containing all X coordinates
+                    } // END horizontal LOOP
                 } // END vertical LOOP
-            } // END horizontal LOOP
-        shuffleArray(arrayYPos);                                                  // randomize order of Y positions (to allow for random dot placement)
-        // window.alert(arrayYPos);                                               // TEST
+            // window.alert(arrayXPos);                                               // TEST
+     
+         // possible Y positions
+            arrayYPos = [];                                                                // create empty array accesisble outside function
+                for (w = 1; w < (horizontalCirckles + 1); w++) {           // repeat Y coordinates for all coordinates of X
+                     for (l = 1; l < (verticalCirckles + 1); l++) {                // positions for the amount of verticalCirckles specified (+1 because l = 1)
+                        var posY = ((3 * sizeCirckel) * l);                         // change depending on circkelSize
+                        arrayYPos.push(posY);                                       // append new position to array containing all Y position
+                    } // END vertical LOOP
+                } // END horizontal LOOP
+            //  window.alert(arrayYPos);                                               // TEST
+            
+            // create unique combination of XY coordinates
+                 arrayYPos.sort();
+        } // END positionGrid FUNCTION
+    
+      // SET RANDOM XY POSITIONS
+            positionGrid (verticalCirckles, horizontalCirckles, sizeCirckel); // call upon positionGrid function
+            
+            // shuffle XY positions
+                var arrayIndex = [];                                                                     // create new Array for random indexing out of XY coordinate arrays
+                    for (y = 1; y < (horizontalCirckles * verticalCirckles); y++) {   // create integers for each point in the grid (1 - ...) 
+                        arrayIndex.push(y);                                                             // append integer to arrayIndex
+                    } // END for LOOP
+                shuffleArray(arrayIndex);                                                             // shuffle order of integers to allow for random indexing (i.e. dot placement)
+                //  window.alert(arrayIndex);
       
-    // DRAW DOTS
-      for (t = 0; t < dots; t++) {
-      // access random coordinates  
-        var X = arrayXPos[t];
-        var Y = arrayYPos[t];
+     // DRAW DOTS
+      for (t = 0; t < dots; t++) {                                          // draw 3 / 4 / 5 dots
+            var index = arrayIndex[t];                                  // random integer used to index from possible X and Y coordinate arrays
+            // access randomly selected XY coordinates  
+            var X = arrayXPos[index];                                // random - but unique - X coordinate
+            var Y = arrayYPos[index];                                 // random - but unique - Y coordinate
   
-        blackDot(X,Y, sizeCirckel);
-      } // END DRAWING DOTS           
+            blackDot(X,Y, sizeCirckel);                                // draw DOT in random placement
+      } // END DRAWING DOTS       
 } // END  randomFigure FUNCTION 
       
 // TEST: INITIALIZE randomFigure FUNCTION
