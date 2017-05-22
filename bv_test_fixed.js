7d10ebe8dfe4377cccfc457c9ac448d8ef02ef2d
SHogenboom <hogen.boom@hotmail.com>
2017-05-22 19:14:14 +0200

    create documentation

---
+++ bv_test_fixed.js
@@ -0,0 +1,55 @@
+﻿/** Bourdon Vos Test
+* @author hogen.boom@hotmail.com (Hogenboom, S. A. M.)
+* @fileoverview Flow order: index > bv_pratice > bv_test > bv_results
+* @todo 
+*/
+
+/*window.onload = requestFullScreen();
+
+// SET WINDOW SIZE
+function setWindow ()
+{
+    // Determine size of screen
+        var screenWidth = screen.availWidth;
+        var screenHeight = screen.availHeight;  
+        
+        window.resizeTo(screenWidth, screenHeight);
+        window.focus;
+}    // END setWindow FUNCTION
+*/
+/*
+function requestFullScreen(element) {
+    // Supports most browsers and their versions.
+    var requestMethod = element.requestFullScreen || element.webkitRequestFullScreen || element.mozRequestFullScreen || element.msRequestFullScreen;
+
+    if (requestMethod) { // Native full screen.
+        requestMethod.call(element);
+    } else if (typeof window.ActiveXObject !== "undefined") { // Older IE.
+        var wscript = new ActiveXObject("WScript.Shell");
+        if (wscript !== null) {
+            wscript.SendKeys("{F11}");
+        }
+    }
+}
+
+var elem = document.body; // Make the body go full screen.
+requestFullScreen(elem);
+        
+*/
+function requestFullScreen(el) {
+            // Supports most browsers and their versions.
+            var requestMethod = el.requestFullScreen || el.webkitRequestFullScreen || el.mozRequestFullScreen || el.msRequestFullscreen;
+
+            if (requestMethod) { // Native full screen.
+                requestMethod.call(el);
+            } else if (typeof window.ActiveXObject !== "undefined") { // Older IE.
+                var wscript = new ActiveXObject("WScript.Shell");
+                if (wscript !== null) {
+                    wscript.SendKeys("{F11}");
+                }
+            }
+            return false
+}
+
+window.onload = requestFullScreen(el);
+        
